d8ecd40c794ff8d29a00cd6fb993f3dc6a2e23fc
Tennant Shaw <tennantshaw@macs-mbp.lan>
2017-04-20 11:31:54 -0400

    Initial Commit

---
+++ TicTacToe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TicTacToe
+//
+//  Created by Tennant Shaw on 4/20/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Tennant Shaw on 4/20/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
